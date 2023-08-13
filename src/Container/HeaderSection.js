823f724c2916d43038c82a2d8e9f0ef73e78f5db
Tom Lewis <tomlewis139@gmail.com>
2023-08-13 17:11:45 +0100

    added location, linked api with coords and added location to header section.

--- src/Container/HeaderSection.js
+++ src/Container/HeaderSection.js
@@ -1,19 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text } from 'react-native';
 
-const HeaderSection = () => {
+const getDayAndTime = () => {
+	let fullDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+	let d = new Date()
+	return `${fullDays[d.getDay()]}, ${d.getHours()}:${d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes()} ${d.getHours() > 11 ? 'PM' : 'AM'}`
+}
+
+const HeaderSection = ({ location }) => {
 	return (
 		<>
 			<View style={{ 'flexDirection': 'row', 'justifyContent': 'space-between', 'alignItems': 'center', 'width': '100%', 'marginTop': 20 }}>
 				<Text style={{ 'fontSize': 25, 'fontWeight': 'bold' }}>
-					Location, Location
+					{`${location[0]?.city}, ${location[0]?.region}`}
 				</Text>
 				<Text>
-					Icon
+					=
 				</Text>
 			</View>
 			<View style={{ 'justifyContent': 'flex-start', 'width': '100%', 'marginTop': 5 }}>
 				<Text style={{ 'fontSize': 20, 'color': '#919191' }}>
-					Day, Time
+					{getDayAndTime()}
 				</Text>
 			</View>
 		</>
--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,23 +1,54 @@
-import React, { useEffect } from 'react'
-import { View, Text } from 'react-native'
-import { HeaderSection, CurrentWeather, ForecastSection } from './Container'
-import getWeather from './api/getWeather'
+import React, { useState, useEffect } from 'react';
+import { ActivityIndicator, Text, View } from 'react-native';
 import { useQuery } from '@tanstack/react-query';
+import getWeather from './api/getWeather';
+import * as Location from 'expo-location';
+import { HeaderSection, CurrentWeather, ForecastSection } from './Container';
 
 const WeatherApp = () => {
 
+	const [coords, setCoords] = useState([])
+	const [location, setLocation] = useState([])
+	const [errorMsg, setErrorMsg] = useState(null)
+
+	useEffect(() => {
+		const getLocation = async () => {
+			const { status } = await Location.requestForegroundPermissionsAsync()
+			if (status !== 'granted') {
+				setErrorMsg('Permission to access location denied.')
+			}
+			const loc = await Location.getCurrentPositionAsync()
+			setCoords([loc.coords.latitude, loc.coords.longitude])
+
+			const regionName = await Location.reverseGeocodeAsync({
+				longitude: loc.coords.longitude,
+				latitude: loc.coords.latitude
+			})
+			setLocation(regionName)
+		}
+		getLocation()
+	}, [])
+
 	const { data, isLoading, isError } = useQuery({
-		queryKey: ['weather'],
-		queryFn: getWeather,
+		queryKey: ['weather', coords],
+		queryFn: async () => {
+			const data = await getWeather(coords)
+			return data
+		},
 	})
+
+	console.log(data)
 
 	return (
 		<View style={{ 'backgroundColor': '#fff', 'alignItems': 'center', 'padding': 20, 'height': 750, }}>
 			{isLoading ? <ActivityIndicator size='large' style={{ 'marginTop': 300 }} /> : null}
-			{isError ? <Text style={{ 'marginTop': 300 }}>Couldn't load weather.</Text> : null}
-			{(data &&
+			{isError ? <Text style={{ 'marginTop': 300 }}>Couldn't load weather.</Text> :
+				errorMsg !== null ? <Text style={{ 'marginTop': 300 }}>{errorMsg}</Text> : null}
+			{(data && !errorMsg &&
 				<>
-					<HeaderSection />
+					<HeaderSection
+						location={location}
+					/>
 					<CurrentWeather
 						src={data?.current?.weather[0]?.icon}
 						temp={Math.round(data?.current?.temp)}
@@ -33,3 +64,4 @@ const WeatherApp = () => {
 }
 
 export default WeatherApp
+
