28f61524ee7a8545dca9f02e5d889ae36724c5f7
Tom Lewis <tomlewis139@gmail.com>
2023-08-13 14:12:06 +0100

    added dotenv, set up api

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Text } from 'react-native'
 import { HeaderSection, CurrentWeather, ForecastSection } from './Container'
+import getWeather from './api/getWeather'
 
 const WeatherApp = () => {
+
+	const [data, setData] = useState({})
+
+	useEffect(() => {
+		let data = getWeather()
+		setData(data)
+	}, [])
+
+	console.log(data)
+
 	return (
 		<View style={{ 'backgroundColor': '#fff', 'alignItems': 'center', 'padding': 20, 'height': 750, }}>
 			<HeaderSection />
---
+++ src/api/getWeather.js
@@ -0,0 +1,14 @@
+import { REACT_APP_WEATHER } from "@env";
+
+const api = {
+    key: REACT_APP_WEATHER,
+    base: 'https://api.openweathermap.org/data/3.0/',
+}
+
+const getWeather = async () => {
+    const response = await fetch(`${api.base}onecall?lat=51.4545&lon=-2.5879&units=metric&exclude=hourly,minutely&appid=${api.key}`)
+    const result = await response.json()
+    return result
+}
+
+export default getWeather
